59ae0c215605132ecbfd49a308079336dbec73e6
shirocodes <ishirapt@gmail.com>
2025-02-06 11:35:49 +0300

    Added html upto .calculator with four inputs and added minimal css. Now I move into addig other features

---
+++ calc.js
@@ -0,0 +1,29 @@
+
+const add = function(num1,num2) {
+    return (num1 + num2);
+    
+};
+
+const substract = function(num1,num2) {
+    return (num1 - num2);
+
+};
+
+const multiply = function(num1,num2) {
+    return (num1*num2);
+
+};
+
+const divide = function(num1,num2) {
+    return (num1/num2);
+
+};
+
+let num1= 0;
+let num2 = 0;
+let operator = "+"; "-"; "*"; "/";
+
+function operate(num1, num2, operator) {
+    return num1
+
+};
